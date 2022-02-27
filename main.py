290c7147bd553883f433266773ada651e1caab95
cajewell01 <99988923+cajewell01@users.noreply.github.com>
2022-02-27 20:15:21 +0000

    Added a new comment at the top for a change I would like to make as I learn more.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Come back and use a loop to repeat if they don't like the name
 #1. Create a greeting for your program.
 print("Welcome to the Band Name Generator!")
 #2. Ask the user for the city that they grew up in.
