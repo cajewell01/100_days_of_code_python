d11b8f109c0a1f2e829196576f0bda23a355a84a
cajewell01 <99988923+cajewell01@users.noreply.github.com>
2022-03-02 14:06:08 -0600

    Add files via upload

--- day-3-1-exercise.py
+++ day-3-1-exercise.py
@@ -1,3 +1,5 @@
+#Write a program that works out whether if a given number is an odd or even number.
+
 # 🚨 Don't change the code below 👇
 number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
